5dba7146edd9617238583eb4a0dac71e2c22674d
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-04-25 22:42:17 +0200

    Button Bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // index.js
 import React from 'react';
 import { render } from 'react-dom';
-
-import ButtonDark from './src/components/dumbComponents/ButtonJoke';
 import { Button } from 'react-bootstrap';
 
 
@@ -11,9 +9,13 @@ class Index extends React.Component{
     
     render(){
         return(
-           <div> 
-                <Button variant="danger" className='mx-auto d-block m-3 btn btn-outline-success text-white'>Button</Button>
-           </div>
+           <section className='p-5 text-center'>  
+                <h1 className='mb-5 bg-dark p-5 text-white'>Les classes <b className='text-danger'>boutons</b></h1>
+                <Button variant="dark" className='m-1'>Button</Button>
+                <Button variant="light" className='btn btn-outline-danger m-1'>Button</Button>
+                <Button variant='primary' className='btn btn-sm m-1'>Button Small</Button>
+                <Button variant='success' className ='btn btn-lg btn-block mt-1'>Large button</Button>
+           </section>
         )
     }
 }
